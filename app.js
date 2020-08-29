8e85339e01d3076554f991a442cc4e0bce2393ff
Jin <s87160204@gmail.com>
2020-08-29 21:09:05 +0800

    Feat:add generate function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const trashtalk = require('./trashtalk')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -17,12 +18,28 @@ app.get('/', (req, res) => {
 })
 
 
-app.post('/', (req, res) => {
-  const target = req.body.target
-  console.log(target)
-  res.render('index')
-})
 
+app.post('/', (req, res) => {
+  const options = req.body
+  const trashtalkSentence = trashtalk(options)
+  // if (target === 'engineer') {
+  //   const trashtalk = '身為一個' + target + random(task.engineer) + random(phrase)
+  //   res.render('index', { trashtalk: trashtalk })
+  // }
+  // if (target === 'designer') {
+  //   const trashtalk = '身為一個' + target + random(task.designer) + random(phrase)
+  //   res.render('index', { trashtalk: trashtalk })
+
+  // }
+  // if (target === 'entrepreneur') {
+  //   const trashtalk = '身為一個' + target + random(task.entrepreneur) + random(phrase)
+  //   res.render('index', { trashtalk: trashtalk })
+  // }
+  res.render('index', {
+    trashtalkSentence
+  })
+
+})
 
 app.listen(port, () => {
   console.log(`The trash talk generator is running on http://localhost:${port}`)
